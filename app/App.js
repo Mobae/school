4c8431d270a404a3825248e658b9414569c91aea
sageOMEGA <abhishekdhakad61@gmail.com>
2020-09-01 11:20:18 +0530

    added react-native-paper

--- app/App.js
+++ app/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello WOrld</Text>
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log('Pressed')}
+      >
+        Press me
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
