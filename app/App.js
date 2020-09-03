bafdcaf6a9f1c4690acf4f7604c893f74cf46c98
AA <adi.2002.gwl@gmail.com>
2020-09-04 00:41:16 +0530

    individual months attendane added

--- app/App.js
+++ app/App.js
@@ -5,12 +5,14 @@ import Profile from './components/profile/Profile';
 import AllStudentsAttendance from './components/profile/AllStudentsAttendance';
 import AddAttendence from './components/profile/AddAttendence';
 import StudentAttendance from './components/profile/StudentView/StudentAttendance';
+import IndividualMonth from './components/profile/StudentView/IndividualMonth';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Profile /> */}
-      <StudentAttendance />
+      {/* <StudentAttendance /> */}
+      <IndividualMonth />
       {/* <AllStudentsAttendance /> */}
       {/* <AddAttendence /> */}
       <StatusBar style='auto' />
---
+++ app/components/profile/StudentView/IndividualMonth.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const IndividualMonth = () => {
+  const [Head, setHead] = React.useState(['Date', 'Present']);
+  const icon = (
+    <Icon name='done' size={30} color='green' style={{ alignSelf: 'center' }} />
+  );
+  const [data, setData] = React.useState([
+    ['01-01-2020', icon],
+    ['02-01-2020', icon],
+    ['03-01-2020', icon],
+    ['04-01-2020', icon],
+  ]);
+  return (
+    <React.Fragment>
+      <View style={{ marginLeft: 155, marginTop: 50 }}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            textDecorationLine: 'underline',
+          }}
+        >
+          January
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+          <Row data={Head} style={styles.head} textStyle={styles.text} />
+          <Rows data={data} textStyle={styles.text} />
+        </Table>
+      </View>
+    </React.Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 15, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6, alignSelf: 'center' },
+});
+
+export default IndividualMonth;
