3dcfb23e6e07b20b87c7b96c282fc99b8a4d71c5
AA <adi.2002.gwl@gmail.com>
2020-09-05 00:22:35 +0530

    alignment done

--- app/components/profile/AddAttendence.js
+++ app/components/profile/AddAttendence.js
@@ -45,7 +45,7 @@ const AddAttendence = () => {
             </Dialog>
           </Portal>
         </View>
-        <View >
+        <View>
           <DatePicker
             style={{
               width: 190,
@@ -61,7 +61,7 @@ const AddAttendence = () => {
             maxDate='31-12-2050'
             confirmBtnText='Confirm'
             cancelBtnText='Cancel'
-            // iconSource={(uri = require('../../calender.png'))}
+            iconSource={(uri = require('../../calender.png'))}
             customStyles={{
               dateIcon: {
                 position: 'absolute',
