e288bf8c2bc041f81c77763478b7c2b52a39af58
AA <adi.2002.gwl@gmail.com>
2020-09-05 00:22:13 +0530

    alignment done

--- app/App.js
+++ app/App.js
@@ -3,16 +3,12 @@ import { StyleSheet } from 'react-native';
 
 import AuthContextProvider from './context/AuthContext';
 import Main from './components/Main';
-import AllStudentsAttendance from './components/profile/AllStudentsAttendance';
-import IndividualMonth from './components/profile/StudentView/IndividualMonth';
-import AddAttendence from './components/profile/AddAttendence';
 
 export default function App() {
   return (
-    // <AuthContextProvider>
-    //   <Main />
-    // </AuthContextProvider>
-    <AddAttendence />
+    <AuthContextProvider>
+      <Main />
+    </AuthContextProvider>
   );
 }
 
