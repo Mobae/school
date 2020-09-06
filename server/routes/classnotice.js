199e31959c82067c881197c6edbd0b72de68eb63
zaryanz <aryanx404@gmail.com>
2020-09-06 20:38:41 +0530

    heorku testing

--- server/routes/classnotice.js
+++ server/routes/classnotice.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
     const notices = ClassNotice.find({ class: class_ });
     res.status(200).json({ notices });
   } catch (err) {
-    res.status(500).json({ err: "Server error" });
+    res.status(500).json({ err });
   }
 });
 
