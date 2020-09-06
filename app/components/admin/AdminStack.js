1c2e6f17457924da7abfaef212b05049acab88e8
zaryanz <aryanx404@gmail.com>
2020-09-06 16:42:57 +0530

    navigation error fix

--- app/components/admin/AdminStack.js
+++ app/components/admin/AdminStack.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import AdminProfile from './AdminProfile';
-import ClassList from './ClassList';
-import TeacherList from './TeacherList';
-import StudentList from './StudentList';
+import AdminProfile from "./AdminProfile";
+import ClassList from "./ClassList";
+import TeacherList from "./TeacherList";
+import StudentList from "./StudentList";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
 function AdminStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="AdminProfile" component={AdminProfile} />
-      <Stack.Screen name="ClassList" component={ClassList} />
-      <Stack.Screen name="TeacherList" component={TeacherList} />
-      <Stack.Screen name="StudentList" component={StudentList} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="AdminProfile" component={AdminProfile} />
+        <Stack.Screen name="ClassList" component={ClassList} />
+        <Stack.Screen name="TeacherList" component={TeacherList} />
+        <Stack.Screen name="StudentList" component={StudentList} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default AdminStack;
