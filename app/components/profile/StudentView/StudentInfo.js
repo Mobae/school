0d6bc9d581589345365a8d5651f6850ac987a10f
AA <adi.2002.gwl@gmail.com>
2020-09-07 01:03:30 +0530

    save button done

--- app/components/profile/StudentView/StudentInfo.js
+++ app/components/profile/StudentView/StudentInfo.js
@@ -1,164 +1,179 @@
-import React, { Fragment } from 'react';
+import * as React from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
+  Button,
   ImageBackground,
   ScrollView,
 } from 'react-native';
-import { List } from 'react-native-paper';
+import { List, Provider as PaperProvider } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 const StudentInfo = () => {
   return (
-    <Fragment>
-      <View style={{ alignItems: 'center', margin: 20 }}>
-        <View style={styles.info}>
-          <ImageBackground
-            source={{
-              uri: 'https://api.adorable.io/avatars/80/abott@adorable.png',
-            }}
-            style={{ height: 100, width: 100 }}
-            imageStyle={{ borderRadius: 15 }}
-          />
+    <React.Fragment>
+      <PaperProvider>
+        <View style={{ alignItems: 'center', margin: 20 }}>
+          <View style={styles.info}>
+            <ImageBackground
+              source={{
+                uri: 'https://api.adorable.io/avatars/80/abott@adorable.png',
+              }}
+              style={{ height: 100, width: 100 }}
+              imageStyle={{ borderRadius: 15 }}
+            />
+          </View>
+          <Text style={{ marginTop: 10, fontSize: 18, fontWeight: 'bold' }}>
+            Aditya Yadav
+          </Text>
         </View>
-        <Text style={{ marginTop: 10, fontSize: 18, fontWeight: 'bold' }}>
-          Aditya Yadav
-        </Text>
-      </View>
 
-      <Fragment>
-        <ScrollView>
-          <View style={styles.info}>
-            <Text style={styles.details}>Class:</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.details}>Admission No.:</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.details}>DOB:</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.details}>Buss Route:</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.details}>Aadhar No.:</Text>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.details}>Date of Addmission:</Text>
-          </View>
-          <List.Accordion
-            title='Parent Details'
-            titleStyle={{ alignSelf: 'flex-start' }}
-            left={(props) => <List.Icon {...props} icon='account-child' />}
-          >
-            <ScrollView>
-              <View style={styles.info}>
-                <View style={{ flexDirection: 'column' }}>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      fontWeight: '500',
-                      fontWeight: 'bold',
-                    }}
-                  >
-                    Father Details:
-                  </Text>
-                  <Text
-                    style={{
-                      marginLeft: 20,
-                    }}
-                  >
-                    Name
-                  </Text>
-                  <Text style={{ marginLeft: 20 }}>Number</Text>
+        <React.Fragment>
+          <ScrollView>
+            <View style={styles.info}>
+              <Text style={styles.details}>Class:</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.details}>Admission No.:</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.details}>DOB:</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.details}>Buss Route:</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.details}>Aadhar No.:</Text>
+            </View>
+            <View style={styles.info}>
+              <Text style={styles.details}>Date of Addmission:</Text>
+            </View>
+            <List.Accordion
+              title='Parent Details'
+              titleStyle={{ alignSelf: 'flex-start' }}
+              left={(props) => <List.Icon {...props} icon='account-child' />}
+            >
+              <ScrollView>
+                <View style={styles.info}>
+                  <View style={{ flexDirection: 'column' }}>
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        fontWeight: '500',
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      Father Details:
+                    </Text>
+                    <Text
+                      style={{
+                        marginLeft: 20,
+                      }}
+                    >
+                      Name
+                    </Text>
+                    <Text style={{ marginLeft: 20 }}>Number</Text>
+                  </View>
                 </View>
-              </View>
-              <View style={styles.info}>
-                <View style={{ flexDirection: 'column' }}>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      fontWeight: '500',
-                      fontWeight: 'bold',
-                    }}
-                  >
-                    Mother Details:
-                  </Text>
-                  <Text
-                    style={{
-                      marginLeft: 20,
-                    }}
-                  >
-                    Name
-                  </Text>
-                  <Text style={{ marginLeft: 20 }}>Number</Text>
+                <View style={styles.info}>
+                  <View style={{ flexDirection: 'column' }}>
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        fontWeight: '500',
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      Mother Details:
+                    </Text>
+                    <Text
+                      style={{
+                        marginLeft: 20,
+                      }}
+                    >
+                      Name
+                    </Text>
+                    <Text style={{ marginLeft: 20 }}>Number</Text>
+                  </View>
                 </View>
-              </View>
-              <View style={styles.info}>
-                <View style={{ flexDirection: 'column' }}>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      fontWeight: '500',
-                      fontWeight: 'bold',
-                    }}
-                  >
-                    Address:
-                  </Text>
-                  <Text
-                    style={{
-                      marginLeft: 20,
-                    }}
-                  >
-                    GWALIOR
-                  </Text>
+                <View style={styles.info}>
+                  <View style={{ flexDirection: 'column' }}>
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        fontWeight: '500',
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      Address:
+                    </Text>
+                    <Text
+                      style={{
+                        marginLeft: 20,
+                      }}
+                    >
+                      GWALIOR
+                    </Text>
+                  </View>
                 </View>
-              </View>
-            </ScrollView>
-          </List.Accordion>
-          <View style={styles.info}>
-            <View style={{ flexDirection: 'column' }}>
-              <Text>Change Password: </Text>
-              <View style={{ flexDirection: 'row' }}>
-                <MaterialCommunityIcons
-                  name='lock-outline'
-                  size={24}
-                  color='black'
-                  style={{ margin: 10 }}
-                />
-                <TextInput
-                  style={{
-                    margin: 10,
-                    alignSelf: 'flex-start',
-                    marginBottom: 0,
-                  }}
-                  autoCapitalize='none'
-                  placeholder='Enter new password'
-                />
-              </View>
-              <View style={{ flexDirection: 'row' }}>
-                <MaterialCommunityIcons
-                  name='lock-outline'
-                  size={24}
-                  color='black'
-                  style={{ margin: 10 }}
-                />
-                <TextInput
-                  style={{
-                    margin: 10,
-                    alignSelf: 'flex-start',
-                    marginBottom: 0,
-                  }}
-                  autoCapitalize='none'
-                  placeholder='Confirm password'
-                />
+              </ScrollView>
+            </List.Accordion>
+            <View style={styles.info}>
+              <View style={{ flexDirection: 'column' }}>
+                <Text>Change Password: </Text>
+                <View style={{ flexDirection: 'row' }}>
+                  <MaterialCommunityIcons
+                    name='lock-outline'
+                    size={24}
+                    color='black'
+                    style={{ margin: 10 }}
+                  />
+
+                  <TextInput
+                    style={{
+                      margin: 10,
+                      alignSelf: 'flex-start',
+                      marginBottom: 0,
+                    }}
+                    autoCapitalize='none'
+                    secureTextEntry={true}
+                    keyboardType='visible-password'
+                    placeholder='Enter new password'
+                  />
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                  <MaterialCommunityIcons
+                    name='lock-outline'
+                    size={24}
+                    color='black'
+                    style={{ margin: 10 }}
+                  />
+                  <TextInput
+                    style={{
+                      margin: 10,
+                      alignSelf: 'flex-start',
+                      marginBottom: 0,
+                    }}
+                    secureTextEntry={true}
+                    autoCapitalize='none'
+                    keyboardType='visible-password'
+                    placeholder='Confirm password'
+                  />
+                </View>
               </View>
             </View>
-          </View>
-        </ScrollView>
-      </Fragment>
-    </Fragment>
+            <View style={{ margin: 20, marginHorizontal: 40 }}>
+              <Button
+                mode='contained'
+                title='save'
+                onPress={() => console.log('Pressed')}
+              ></Button>
+            </View>
+          </ScrollView>
+        </React.Fragment>
+      </PaperProvider>
+    </React.Fragment>
   );
 };
 
@@ -181,11 +196,7 @@ const styles = StyleSheet.create({
   fab: {
     position: 'absolute',
     margin: 16,
-    right: 18,
+    right: 0,
     bottom: 0,
-    height: 63,
-    borderRadius: 50,
-    backgroundColor: '#00bfff',
-    width: 63,
   },
 });
