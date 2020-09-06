27b365d22ff9a86ad1b780b33f4d84fa83c4347d
tars <ajaysharma2ku@gmail.com>
2020-09-06 16:48:30 +0530

    fixed conflict

--- app/components/admin/AdminStack.js
+++ app/components/admin/AdminStack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import AdminProfile from './AdminProfile';
 import ClassList from './ClassList';
@@ -10,12 +11,14 @@ const Stack = createStackNavigator();
 
 function AdminStack() {
   return (
+    <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="AdminProfile" component={AdminProfile} />
       <Stack.Screen name="ClassList" component={ClassList} />
       <Stack.Screen name="TeacherList" component={TeacherList} />
       <Stack.Screen name="StudentList" component={StudentList} />
     </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
