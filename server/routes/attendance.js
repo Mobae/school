adaeda39e95b830295a4faeb8b8f0980806aa85e
zaryanz <aryanx404@gmail.com>
2020-09-07 17:28:01 +0530

    aggregate trial

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -58,7 +58,7 @@ router.get("/student/:id/:month", auth, async (req, res) => {
 router.get("/months", async (req, res) => {
   const agg = await Attendance.aggregate([
     { $project: { month: { $month: "$date" } } },
-    { $group: { _id: { month: "$month" } } },
+    { $group: { _id: { month: "$month", studentId: "$studentId" } } },
   ]);
   console.log(agg);
 });
