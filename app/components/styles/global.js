dee8b9c3e911e8fac1849a3c486a65a963ab19e2
zaryanz <aryanx404@gmail.com>
2020-09-08 11:11:13 +0530

    notice browse

--- app/components/styles/global.js
+++ app/components/styles/global.js
@@ -5,10 +5,20 @@ const styles = StyleSheet.create({
   view: {
     margin: 12,
   },
-  headline: {
-    margin: 12,
-    marginTop: 15,
-    fontSize: 30,
+  card: {
+    margin: 10,
+    marginTop: 13,
+    marginBottom: 0,
+  },
+  cardContent: {
+    marginLeft: 5,
+    paddingBottom: 10,
+  },
+  cardP: {
+    color: "#606060",
+  },
+  cardButton: {
+    marginLeft: "auto",
   },
 });
 
