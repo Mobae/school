07eb84dbcf666b96838d20e9c0d26c3e241f4b7e
zaryanz <aryanx404@gmail.com>
2020-09-08 11:50:52 +0530

    get student info

--- server/routes/student.js
+++ server/routes/student.js
@@ -45,9 +45,9 @@ router.get("/teachers/all", async (req, res) => {
   }
 });
 
-router.get("/", auth, async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   try {
-    const student = await Student.findById(req.body.data.id);
+    const student = await Student.findById(req.params.id);
     return res.json({
       student,
     });
