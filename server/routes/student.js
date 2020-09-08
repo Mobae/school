d21f48ec9b43ed16b0ba44607d07d3862489f7c3
zaryanz <aryanx404@gmail.com>
2020-09-08 21:46:12 +0530

    added new initial student get

--- server/routes/student.js
+++ server/routes/student.js
@@ -45,6 +45,13 @@ router.get("/teachers/all", async (req, res) => {
   }
 });
 
+router.get("/initial", auth, (req, res) => {
+  const student = await Student.findById(req.body.data.id);
+  return res.json({
+    student,
+  });
+})
+
 router.get("/:id", auth, async (req, res) => {
   try {
     console.log(`STUDENT ID: ${req.params.id}`);
