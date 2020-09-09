357ad3a83a7d5c1a8f90dc95a64815c61e343661
AA <adi.2002.gwl@gmail.com>
2020-09-09 16:08:29 +0530

    teacher files started

--- app/components/Chats/Files.js
+++ app/components/Chats/Files.js
@@ -1,54 +1,78 @@
 import * as React from 'react';
-import { Card, Title, Paragraph } from 'react-native-paper';
-import { View, TouchableOpacity } from 'react-native';
+import { Card, Title, Paragraph, FAB, Button } from 'react-native-paper';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Files = () => {
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        marginHorizontal: 20,
-        marginVertical: 20,
-      }}
-    >
-      <TouchableOpacity>
-        <Card style={{ marginTop: 10, backgroundColor: '#eee' }}>
-          <Card.Content>
-            <Title>File 1</Title>
-            <Paragraph>Description</Paragraph>
-            <View style={{ flexDirection: 'row' }}>
-              <Paragraph>Date:</Paragraph>
-              <Paragraph style={{ marginLeft: 160 }}>Size:</Paragraph>
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Card style={{ marginTop: 10, backgroundColor: '#eee' }}>
-          <Card.Content>
-            <Title>File 2</Title>
-            <Paragraph>Description</Paragraph>
-            <View style={{ flexDirection: 'row' }}>
-              <Paragraph>Date:</Paragraph>
-              <Paragraph style={{ marginLeft: 160 }}>Size:</Paragraph>
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Card style={{ marginTop: 10, backgroundColor: '#eee' }}>
-          <Card.Content>
-            <Title>File 3</Title>
-            <Paragraph>Description</Paragraph>
-            <View style={{ flexDirection: 'row' }}>
-              <Paragraph>Date:</Paragraph>
-              <Paragraph style={{ marginLeft: 160 }}>Size:</Paragraph>
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-    </View>
+    <React.Fragment>
+      <Button
+        icon='attachment'
+        mode='contained'
+        onPress={() => console.log('Pressed')}
+        style={styles.attach}
+      >
+        Attach Files
+      </Button>
+
+      <View
+        style={{
+          justifyContent: 'center',
+          marginHorizontal: 20,
+          marginVertical: 20,
+        }}
+      >
+        <TouchableOpacity>
+          <Card style={{ marginTop: 10, backgroundColor: '#eee' }}>
+            <Card.Content>
+              <Title>File 1</Title>
+              <Paragraph>Description</Paragraph>
+              <Paragraph>Subject:</Paragraph>
+              <View style={{ flexDirection: 'row' }}>
+                <Paragraph>Date:</Paragraph>
+                <Paragraph style={{ marginLeft: 160 }}>Size:</Paragraph>
+              </View>
+            </Card.Content>
+          </Card>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Card style={{ marginTop: 10, backgroundColor: '#eee' }}>
+            <Card.Content>
+              <Title>File 2</Title>
+              <Paragraph>Description</Paragraph>
+              <Paragraph>Subject:</Paragraph>
+              <View style={{ flexDirection: 'row' }}>
+                <Paragraph>Date:</Paragraph>
+                <Paragraph style={{ marginLeft: 160 }}>Size:</Paragraph>
+              </View>
+            </Card.Content>
+          </Card>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Card style={{ marginTop: 10, backgroundColor: '#eee' }}>
+            <Card.Content>
+              <Title>File 3</Title>
+              <Paragraph>Description</Paragraph>
+              <Paragraph>Subject:</Paragraph>
+              <View style={{ flexDirection: 'row' }}>
+                <Paragraph>Date:</Paragraph>
+                <Paragraph style={{ marginLeft: 160 }}>Size:</Paragraph>
+              </View>
+            </Card.Content>
+          </Card>
+        </TouchableOpacity>
+      </View>
+    </React.Fragment>
   );
 };
 
 export default Files;
+
+const styles = StyleSheet.create({
+  attach: {
+    position: 'absolute',
+    margin: 16,
+    right: 10,
+    bottom: 17,
+    borderRadius: 20,
+  },
+});
