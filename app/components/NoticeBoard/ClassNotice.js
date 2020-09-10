3de1410a2a3a5968069eb6491a0f454c346bcf7d
zaryanz <aryanx404@gmail.com>
2020-09-10 09:51:21 +0530

    notice refactoring

--- app/components/NoticeBoard/ClassNotice.js
+++ app/components/NoticeBoard/ClassNotice.js
@@ -1,58 +1,13 @@
-import React, {
-  Fragment,
-  useRef,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { ScrollView, View } from "react-native";
-import {
-  Card,
-  Avatar,
-  Paragraph,
-  TouchableRipple,
-  Subheading,
-  Headline,
-  IconButton,
-} from "react-native-paper";
-import RBSheet from "react-native-raw-bottom-sheet";
+import React, { Fragment, useContext, useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import { IconButton } from "react-native-paper";
+import axios from "axios";
 
 import { AuthContext } from "../../context/AuthContext";
 import { URL } from "../../config";
 import styles from "./styles";
 
-import axios from "axios";
-
-const NoticeIcon = () => {
-  return <Avatar.Icon icon="bulletin-board" size={45} />;
-};
-
-const NoticeCard = (props) => {
-  const refRBSheet = useRef();
-  let { date } = props;
-  date = new Date(date).toDateString();
-  return (
-    <Fragment>
-      <Card style={styles.notice}>
-        <TouchableRipple onPress={() => refRBSheet.current.open()}>
-          <Fragment>
-            <Card.Title title={props.title} subtitle={date} left={NoticeIcon} />
-            <Card.Content style={{ marginBottom: 8 }}>
-              <Subheading>{props.description}</Subheading>
-              <Paragraph>Issued By: {props.author}</Paragraph>
-            </Card.Content>
-          </Fragment>
-        </TouchableRipple>
-      </Card>
-      <RBSheet ref={refRBSheet} closeOnDragDown={true} height={500}>
-        <View style={styles.notice}>
-          <Headline style={styles.headline}>{props.title}</Headline>
-          <Paragraph>{props.description}</Paragraph>
-        </View>
-      </RBSheet>
-    </Fragment>
-  );
-};
+import NoticeCard from "./NoticeCard";
 
 const ClassNotice = ({ navigation }) => {
   const [notices, setNotices] = useState([]);
