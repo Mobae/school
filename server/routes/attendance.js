1447f479cd91a08489046e213f8f09aca1363ed2
zaryanz <aryanx404@gmail.com>
2020-09-10 14:53:03 +0530

    fix

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -63,6 +63,7 @@ router.get("/class/:classId", async (req, res) => {
     const data = cls.students.forEach(async (stu) => {
       try {
         const det = await Student.findById(stu._id);
+        console.log(det);
         const att = await Attendance.find({ studentId: stu._id });
         return { name: det.name, rollNo: det.rollNo, attendance: att };
       } catch (err) {
