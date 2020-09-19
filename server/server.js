d7d83edd0f8aa89b86a098c3b8bae5e42bf4b57f
tars <ajaysharma2ku@gmail.com>
2020-09-19 22:43:37 +0530

    Add

--- server/server.js
+++ server/server.js
@@ -26,19 +26,19 @@ const storage = new GridFsStorage({
   url: process.env.MONGO_URI,
   file: (req, file) => {
     console.log(file);
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads',
-        };
-        resolve(fileInfo);
-      });
-    });
+    // return new Promise((resolve, reject) => {
+    //   crypto.randomBytes(16, (err, buf) => {
+    //     if (err) {
+    //       return reject(err);
+    //     }
+    //     const filename = buf.toString('hex') + path.extname(file.originalname);
+    //     const fileInfo = {
+    //       filename: filename,
+    //       bucketName: 'uploads',
+    //     };
+    //     resolve(fileInfo);
+    //   });
+    // });
   },
 });
 const upload = multer({ storage });
