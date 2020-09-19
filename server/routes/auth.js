4610256c0822562457981dde3ff5ecfbd8db935d
zaryanz <aryanx404@gmail.com>
2020-09-19 17:37:19 +0530

    fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,7 +7,7 @@ const auth = require("../middleware/auth");
 router.get("/login", (req, res) => {
   const token = req.get("auth-token");
   const { data } = jwt.verify(token, process.env.JWT_SECRET);
-  if (data) {
+  if (data.id) {
     res.json({ success: "true" });
     console.log(data);
   } else {
