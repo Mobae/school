9ca1ecf5879af9e8b1f6868e999f703a61c0068a
zaryanz <aryanx404@gmail.com>
2020-09-19 17:26:54 +0530

    fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,28 +7,27 @@ const Admin = require("../models/Admin");
 module.exports = async function (req, res, next) {
   const token = req.get("auth-token");
   console.log(token);
-  try {
-    const { data } = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (err) {
-    console.log(err);
-    console.log("jwt not verified");
-  }
-  const id = data.id;
-  let user = await Student.findById(id);
-  if (user) {
-    req.body.data = user;
-    next();
-  }
-  user = await Teacher.findById(id);
-  if (user) {
-    req.body.data = user;
-    console.log(user, "hi");
-    next();
-  }
-  user = await Admin.findById(id);
-  if (user) {
-    req.body.data = user;
-    console.log(user);
-    next();
+  const { data } = jwt.verify(token, process.env.JWT_SECRET);
+  if (data) {
+    const id = data.id;
+    let user = await Student.findById(id);
+    if (user) {
+      req.body.data = user;
+      next();
+    }
+    user = await Teacher.findById(id);
+    if (user) {
+      req.body.data = user;
+      console.log(user, "hi");
+      next();
+    }
+    user = await Admin.findById(id);
+    if (user) {
+      req.body.data = user;
+      console.log(user);
+      next();
+    }
+  } else {
+    res.json({ error: "No user exists" });
   }
 };
