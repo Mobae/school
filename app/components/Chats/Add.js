0540baffc016f0607612acfd9b89017560c1db88
tars <ajaysharma2ku@gmail.com>
2020-09-20 14:43:19 +0530

    comil

--- app/components/Chats/Add.js
+++ app/components/Chats/Add.js
@@ -3,23 +3,22 @@ import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 import axios from "axios";
 
-// import DocumentPicker from 'react-native-document-picker';
-
-import * as DocumentPicker from "expo-document-picker";
+import DocumentPicker from 'react-native-document-picker';
+// import * as DocumentPicker from "expo-document-picker";
 
 const Add = () => {
   const [file, setFile] = React.useState(null);
 
   const uploadFile = async () => {
     try {
-      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&              file :      ');
       console.log(file);
       const data = new FormData();
       data.append("classId", "5f6252205181b70004f5d909");
       data.append("teacherId", "5f6250b55181b70004f5d906");
       data.append("caption", "from app");
       data.append("file", file);
-      console.log(data);
+      console.log(`##############################3 data: ${data}`);
       const config = {
         headers: {
           "Content-Type": "multipart/form-data; ",
@@ -29,7 +28,8 @@ const Add = () => {
 
       const res = await axios.post(url, data);
       // const res = await axios.get(url);
-      // console.log(res.data);
+      console.log(res.data);
+
     } catch (err) {
       console.log(err);
     }
@@ -37,10 +37,19 @@ const Add = () => {
 
   const selFile = async () => {
     try {
-      DocumentPicker.getDocumentAsync().then((res) => {
-        console.log(res);
-        setFile(res);
-      });
+      // DocumentPicker.getDocumentAsync().then((res) => {
+      //   console.log(res);
+      //   setFile(res);
+      // });
+
+    const res = await DocumentPicker.pick({
+      type: [DocumentPicker.types.allFiles],
+    });
+
+    console.log('res : ' + JSON.stringify(res));
+
+    setFile(res);
+
     } catch (error) {
       console.log(error);
     }
