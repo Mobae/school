fd5e1dccb6362e95f96647dc2f8ff774b0dd2171
tars003 <48816808+tars003@users.noreply.github.com>
2020-09-20 16:30:04 +0530

    Update student.js

--- server/routes/student.js
+++ server/routes/student.js
@@ -47,7 +47,7 @@ router.get("/teachers/all", async (req, res) => {
 
 router.get("/initial", auth, async (req, res) => {
   try {
-    console.log("Entered students !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"):
+    console.log("Entered students !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     console.log(req.body.data);
     let student = await Student.findById(req.body.data.id);
     if (student) {
