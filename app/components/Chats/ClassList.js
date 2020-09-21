89e61afb269f9e72c55e83dd7e0fad631c144d81
zaryanz <aryanx404@gmail.com>
2020-09-21 20:03:35 +0530

    class list header

--- app/components/Chats/ClassList.js
+++ app/components/Chats/ClassList.js
@@ -1,40 +1,48 @@
-import * as React from 'react';
-import axios from 'axios';
-import { View, StyleSheet,Text } from 'react-native';
+import * as React from "react";
+import axios from "axios";
+import { View, StyleSheet, Text } from "react-native";
 import {
   Avatar,
   Button,
   Card,
   Searchbar,
   TouchableRipple,
-  ActivityIndicator
-} from 'react-native-paper';
-import { ScrollView } from 'react-native-gesture-handler';
+  ActivityIndicator,
+} from "react-native-paper";
+import { ScrollView } from "react-native-gesture-handler";
 
-import { AdminContext } from '../../context/AdminContext';
+import { AdminContext } from "../../context/AdminContext";
+import {} from "../../context/AuthContext";
 const LeftContent = (props) => (
   <Avatar.Icon
     {...props}
-    icon='account-group'
-    style={{ backgroundColor: '#00674D' }}
+    icon="account-group"
+    style={{ backgroundColor: "#00674D" }}
   />
 );
-import adminStyles from '../admin/AdminStyles';
+import adminStyles from "../admin/AdminStyles";
 
 const FileClassList = ({ navigation }) => {
-  const url = 'https://school-server-testing.herokuapp.com';
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const [ classes, setClasses ] = React.useState({});
-  const [ currClass, setCurrClass ] = React.useState('');
+  const url = "https://school-server-testing.herokuapp.com";
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [classes, setClasses] = React.useState({});
+  const [currClass, setCurrClass] = React.useState("");
   const [filtered, setFiltered] = React.useState();
-  const [ flag, setFlag ] = React.useState(false);
+  const [flag, setFlag] = React.useState(false);
   const onChangeSearch = (query) => {
     setSearchQuery(query);
   };
 
   const getClasses = async () => {
+    const {
+      authState: { token },
+    } = React.useContext(AuthContext);
     try {
-      let res = await axios.get(url + '/class/all');
+      let res = await axios.get(url + "/class/all", {
+        headers: {
+          "auth-token": token,
+        },
+      });
       const classes = res.data.data;
       setClasses(classes);
       setFiltered(classes);
@@ -48,11 +56,11 @@ const FileClassList = ({ navigation }) => {
 
   React.useEffect(() => {
     getClasses();
-  }, [])
+  }, []);
 
   React.useEffect(() => {
     console.log(filtered);
-    if (searchQuery === '') {
+    if (searchQuery === "") {
       setFiltered(classes);
     } else {
       setFiltered(
@@ -65,11 +73,11 @@ const FileClassList = ({ navigation }) => {
     }
   }, [searchQuery]);
 
-  if(flag ){
+  if (flag) {
     return (
       <View>
         <Searchbar
-          placeholder='Search class..'
+          placeholder="Search class.."
           onChangeText={onChangeSearch}
           value={searchQuery}
         />
@@ -83,8 +91,8 @@ const FileClassList = ({ navigation }) => {
                       onPress={() => {
                         setCurrClass(class_._id);
                         setFlag(false);
-                        navigation.navigate('TeacherFileView', {
-                          class_: class_._id
+                        navigation.navigate("TeacherFileView", {
+                          class_: class_._id,
                         });
                       }}
                     >
@@ -98,7 +106,7 @@ const FileClassList = ({ navigation }) => {
               ))
             ) : (
               <Card style={adminStyles.card}>
-                <Card.Title title='None' left={LeftContent} />
+                <Card.Title title="None" left={LeftContent} />
               </Card>
             )}
             <Text></Text>
@@ -117,26 +125,26 @@ const FileClassList = ({ navigation }) => {
         />
       </View>
     );
-  }  
+  }
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
     marginHorizontal: 10,
     marginVertical: 10,
   },
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
   },
   loading: {
-    alignSelf: 'center',
+    alignSelf: "center",
   },
 });
 
