7babd397e1ae535d085c99f1cfc1b24534549d4e
AA <adi.2002.gwl@gmail.com>
2020-09-22 21:22:21 +0530

    fix

--- app/components/admin/AdminStack.js
+++ app/components/admin/AdminStack.js
@@ -1,21 +1,21 @@
-import React, { useContext } from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { View } from "react-native";
-import { Text, IconButton } from "react-native-paper";
+import React, { useContext } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View } from 'react-native';
+import { Text, IconButton } from 'react-native-paper';
 
-import AdminProfile from "./AdminProfile";
-import ClassList from "./ClassList";
-import TeacherList from "./TeacherList";
-import StudentList from "./StudentList";
+import AdminProfile from './AdminProfile';
+import ClassList from './ClassList';
+import TeacherList from './TeacherList';
+import StudentList from './StudentList';
 import ClassView from './ClassView';
 import AllStudentList from './AllStudentList';
 import AttendanceClassList from './AttendanceClassList';
 import StudentAttendance from './StudentAttendance';
 import StudentDetail from './StudentDetail';
 
-import { AuthContext } from "../../context/AuthContext";
+import { AuthContext } from '../../context/AuthContext';
 
 const Stack = createStackNavigator();
 
@@ -23,17 +23,17 @@ const LogoutButton = () => {
   const { Logout } = useContext(AuthContext);
   return (
     <TouchableOpacity onPress={() => Logout()}>
-      <View style={{ flexDirection: "row" }}>
+      <View style={{ flexDirection: 'row' }}>
         <Text
           style={{
-            marginTop: "auto",
-            marginBottom: "auto",
-            color: "#ef5350",
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            color: '#ef5350',
           }}
         >
           Logout
         </Text>
-        <IconButton icon="logout" color="#ef5350" style={{ marginLeft: 0 }} />
+        <IconButton icon='logout' color='#ef5350' style={{ marginLeft: 0 }} />
       </View>
     </TouchableOpacity>
   );
@@ -43,30 +43,54 @@ function AdminStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
-          name="AdminProfile" 
+        <Stack.Screen
+          name='AdminProfile'
           component={AdminProfile}
           options={{
             headerRight: () => <LogoutButton />,
-            title: 'Admin Profile'
+            title: 'Admin Profile',
           }}
         />
-        <Stack.Screen name="ClassList" component={ClassList} options={{ title: 'All Classes' }} />
-        <Stack.Screen name="AttendanceClassList" component={AttendanceClassList} options={{ title: 'All Classes' }} />
-<<<<<<< HEAD
-        <Stack.Screen name="TeacherList" component={TeacherList} options={{ title: 'All Teachers' }} />
-        <Stack.Screen name="StudentList" component={StudentList} options={{ title: 'All Students' }} />
-        <Stack.Screen name="AllStudentList" component={AllStudentList} options={{ title: 'All Students' }} />
-        <Stack.Screen name="ClassView" component={ClassView} options={{ title: 'Class Overview' }} />
-        <Stack.Screen name="StudentAttendance" component={StudentAttendance} options={{ title: 'Student Attendance' }} />
-=======
-        <Stack.Screen name="TeacherList" component={TeacherList} options={{ title: 'All Classes' }} />
-        <Stack.Screen name="StudentList" component={StudentList} options={{ title: 'Students' }} />
-        <Stack.Screen name="AllStudentList" component={AllStudentList} options={{ title: 'All Students' }} />
-        <Stack.Screen name="ClassView" component={ClassView} options={{ title: 'Class Overview' }} />
-        <Stack.Screen name="StudentAttendance" component={StudentAttendance} options={{ title: 'Attendance' }} />
->>>>>>> bd0051db8380138dfaeee6b3d17ad6af4fba3f48
-        <Stack.Screen name="Student Details" component={StudentDetail} options={{ title: 'Student Details' }} />
+        <Stack.Screen
+          name='ClassList'
+          component={ClassList}
+          options={{ title: 'All Classes' }}
+        />
+        <Stack.Screen
+          name='AttendanceClassList'
+          component={AttendanceClassList}
+          options={{ title: 'All Classes' }}
+        />
+        <Stack.Screen
+          name='TeacherList'
+          component={TeacherList}
+          options={{ title: 'All Classes' }}
+        />
+        <Stack.Screen
+          name='StudentList'
+          component={StudentList}
+          options={{ title: 'Students' }}
+        />
+        <Stack.Screen
+          name='AllStudentList'
+          component={AllStudentList}
+          options={{ title: 'All Students' }}
+        />
+        <Stack.Screen
+          name='ClassView'
+          component={ClassView}
+          options={{ title: 'Class Overview' }}
+        />
+        <Stack.Screen
+          name='StudentAttendance'
+          component={StudentAttendance}
+          options={{ title: 'Attendance' }}
+        />
+        <Stack.Screen
+          name='Student Details'
+          component={StudentDetail}
+          options={{ title: 'Student Details' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
